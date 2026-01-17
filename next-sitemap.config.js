1a4bd2915e0297470fc6e1178f843690c576e65b
hamza Tanveer <hamza.tanveer.dev@gmail.com>
2026-01-17 09:30:49 +0500

    apply sitemap size

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,6 @@
+// next-sitemap.config.js
 module.exports = {
   siteUrl: 'https://gpa-calculator-com.vercel.app',
   generateRobotsTxt: true,
+  sitemapSize: 5000, // large enough to avoid splitting
 }
